da2f65f0bf9f46c4bd6609babc29ea9f7d02e0c6
thuitaf <faith.thuita@moringaschool.com>
2022-03-02 11:57:13 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+
+    cats.push(name)
+
+} 
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+function appendCat(name){
+    const newCats = [...cats,name];
+
+    return newCats
+}
+function prependCat(name){
+    const newCats = [name,...cats];
+
+    return newCats
+}
+function removeLastCat(name){
+    const removeCat = cats.slice(0, cats.length -1 );
+
+    return removeCat
+    
+}
+function removeFirstCat(name){
+    const removeCat = cats.slice(1);
+
+    return removeCat
+    
+}
